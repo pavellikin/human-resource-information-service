4c0dde1fa0d8eb0355ff1ff9e470affed32bfd27
Pavel Likin <pavellikin@gmail.com>
2024-08-15 10:51:00 +0200

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+}
+
+group = "org.mycompany"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package org.mycompany
+
+fun main() {
+    println("Hello World!")
+}
