3ac3b87a30cc43aa76cf769c797d2de7e8c18b46
Pavel Likin <pavellikin@gmail.com>
2024-08-16 16:22:53 +0200

    Add initial ktor configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+    id("io.ktor.plugin") version "2.3.12"
 }
 
 group = "org.mycompany"
@@ -10,7 +12,27 @@ repositories {
 }
 
 dependencies {
+    // ktor
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-cors")
+    implementation("io.ktor:ktor-server-default-headers")
+    implementation("io.ktor:ktor-server-call-logging")
+    implementation("io.ktor:ktor-server-call-id")
+    implementation("io.ktor:ktor-server-swagger")
+
+    // metrics
+    implementation("io.ktor:ktor-server-metrics-micrometer")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.13.3")
+
+    // di
+    implementation("org.kodein.di:kodein-di-framework-ktor-server-jvm:7.22.0")
+
+    // logs
+    implementation("ch.qos.logback:logback-classic:1.5.7")
+
     testImplementation(kotlin("test"))
+    testImplementation("io.ktor:ktor-server-test-host")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,86 @@
 package org.mycompany
 
-fun main() {
-    println("Hello World!")
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.metrics.micrometer.MicrometerMetrics
+import io.ktor.server.plugins.callid.CallId
+import io.ktor.server.plugins.callid.generate
+import io.ktor.server.plugins.callloging.CallLogging
+import io.ktor.server.plugins.cors.routing.CORS
+import io.ktor.server.plugins.defaultheaders.DefaultHeaders
+import io.ktor.server.plugins.swagger.swaggerUI
+import io.ktor.server.request.receiveText
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics
+import io.micrometer.prometheusmetrics.PrometheusConfig
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
+import org.kodein.di.bind
+import org.kodein.di.instance
+import org.kodein.di.ktor.closestDI
+import org.kodein.di.ktor.di
+import org.kodein.di.singleton
+import org.slf4j.event.Level
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    configureDi()
+    configureServer()
+    configureRoutes()
+}
+
+private fun Application.configureDi() {
+    di {
+        bind<PrometheusMeterRegistry> {  singleton { PrometheusMeterRegistry(PrometheusConfig.DEFAULT) }}
+    }
+}
+
+private fun Application.configureServer() {
+    install(DefaultHeaders)
+    install(CORS) {
+        anyHost()
+        allowHeader(HttpHeaders.ContentType)
+    }
+    val loggingLevel = environment.config.config("logging").property("root")
+        .getString()
+        .let { Level.valueOf(it.uppercase()) }
+    install(CallLogging) {
+        level = loggingLevel
+    }
+    install(CallId) {
+        retrieveFromHeader(HttpHeaders.XRequestId)
+        generate(10, "abcde12345")
+    }
+    val meterRegistry by closestDI().instance<MeterRegistry>()
+    install(MicrometerMetrics) {
+        registry = meterRegistry
+        meterBinders = listOf(
+            JvmMemoryMetrics(),
+            JvmGcMetrics(),
+            ProcessorMetrics()
+        )
+    }
+}
+
+private fun Application.configureRoutes() {
+    val di = closestDI()
+    routing {
+        swaggerUI(path = "openapi", swaggerFile = "openapi/human-resource-information-service.yaml")
+        get("/metrics") {
+            val registry by di.instance<PrometheusMeterRegistry>()
+            call.respond(registry.scrape())
+        }
+        get("/health") {
+            call.respond(HttpStatusCode.OK)
+        }
+    }
 }
